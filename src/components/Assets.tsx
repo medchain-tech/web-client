7642abebc1b55e74b73f7987a17079f0c428abfd
Ikuewumi <ayobamsik@gmail.com>
2024-05-22 13:35:29 +0100

    feat: started working on the navigation

---
+++ src/components/Assets.tsx
@@ -0,0 +1,14 @@
+import { JSXElement } from "solid-js";
+
+export const Assets = (): JSXElement => {
+
+  return (
+
+    <svg style="display: none;">
+      <symbol viewBox="0 0 512 512" id="home"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M80 212v236a16 16 0 0 0 16 16h96V328a24 24 0 0 1 24-24h80a24 24 0 0 1 24 24v136h96a16 16 0 0 0 16-16V212"></path><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M480 256L266.89 52c-5-5.28-16.69-5.34-21.78 0L32 256m368-77V64h-48v69"></path></symbol>
+      <symbol viewBox="0 0 24 24" id="down"><path fill="currentColor" d="M12 14.975q-.2 0-.375-.062T11.3 14.7l-4.6-4.6q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l3.9 3.9l3.9-3.9q.275-.275.7-.275t.7.275t.275.7t-.275.7l-4.6 4.6q-.15.15-.325.213t-.375.062"></path></symbol>
+    </svg>
+
+  )
+
+}
