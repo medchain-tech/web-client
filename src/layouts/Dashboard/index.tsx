9f1a304114f988a50b401be0afc50ee829334606
Ikuewumi <ayobamsik@gmail.com>
2024-06-24 00:05:26 +0100

    feat: added notification component to the sidebar

--- src/layouts/Dashboard/index.tsx
+++ src/layouts/Dashboard/index.tsx
@@ -1,11 +1,53 @@
 import { type JSXElement } from "solid-js"
 import "./index.scss"
+import Notification, { NotificationProps } from "@/components/Notification"
+
+const demoNotification: NotificationProps = {
+  "type": "trends",
+  "date": "2mins ago",
+  "description": "Lorem ipsum intel"
+}
 
 const Dashboard = (): JSXElement => {
 
-  return <>
+  return <div class="dboard">
 
-  </>
+    <div class="dboard-main"></div>
+    <div class="dboard-sidebar">
+      <div class="dboard-notifications">
+        <div class="dboard-notifications-header">
+          <svg viewBox="0 0 256 256"><use href="#bell"></use></svg>
+
+          <h3>Notifications</h3>
+        </div>
+
+        <ul class="dboard-notifications-list">
+          <li class="dboard-notifications-li">
+            <Notification {...demoNotification} />
+          </li>
+          <li class="dboard-notifications-li">
+            <Notification {...demoNotification} />
+          </li>
+          <li class="dboard-notifications-li">
+            <Notification {...demoNotification} />
+          </li>
+          <li class="dboard-notifications-li">
+            <Notification {...demoNotification} />
+          </li>
+        </ul>
+
+
+
+      </div>
+
+    </div>
+
+
+  </div>
+
+
+
+
 
 }
 
---
+++ src/layouts/UserPage/index.tsx
@@ -0,0 +1,39 @@
+import { ParentProps, type JSXElement } from "solid-js";
+import "./index.scss";
+
+
+
+const UserPage = (props: ParentProps): JSXElement => {
+
+	return <section class="upage">
+		<div class="upage-header">
+			<h2 class="upage-header-message">Welcome, Adeola</h2>
+			<form class="upage-header-form">
+				<label for="upage-header-search" class="upage-header-label">
+					<svg viewBox="0 0 24 24">
+						<title>Search!</title>
+						<use href="#search"></use>
+					</svg>
+				</label>
+				<input id="upage-header-search" type="search" class="upage-header-search" placeholder="Search" />
+			</form>
+		</div>
+
+
+		<section class="upage-body">
+			<div class="upage-main">
+				{props.children}
+			</div>
+
+
+			<aside class="upage-sidebar">
+
+
+			</aside>
+		</section>
+
+	</section>
+
+}
+
+export default UserPage
