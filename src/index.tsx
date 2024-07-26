3213a7c8caeac409535f73af8cb832e57c7b3dde
= <=>
2024-07-26 12:49:30 +0100

    fix(Route): new route structure configured for proper routes modification

--- src/index.tsx
+++ src/index.tsx
@@ -1,64 +1,29 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import { render } from "solid-js/web";
 
-import { Router, Route } from '@solidjs/router'
-import './index.scss'
-/* import { lazy } from 'solid-js'
-import { SignUpLayout } from './layouts/SignUp'
-import { Individual } from './pages/SignUp/Individual'
-import { Doctor } from './pages/SignUp/Doctor'
-*/
-import App from './App'
-import { Setup } from './pages/SignUp/Setup'
-import { AccountType } from './types'
-import { Start } from './pages/SignUp/Start'
-import Verification from './pages/SignUp/Verification'
-import Profile from './pages/SignUp/Profile'
-import Hospital from './pages/SignUp/Hospital'
-import Home from './pages/Dashboard/Home'
+import { Router } from "@solidjs/router";
+import "./index.scss";
+import routes from "./routes";
 
-const root = document.getElementById('root')
-/* const Home = lazy(() => import("./pages/Home/index"))
-const DoctorPage = lazy(() => import("./pages/Doctor/index"))
-const Dashboard = lazy(() => import("./layouts/Dashboard/index"))
-const UserLayout = lazy(() => import("./layouts/UserPage/index"))
+const root = document.getElementById("root");
 
-const HospitalHome = lazy(() => import("./pages/HospitalHome/index"))
- */
+render(
+  () => (
+    // <Router>
+    //   <Route path="/" component={AppLayout}>
+    //     {/* <Route path="/home" component={Home} /> */}
+    //     {/* <Route path="/user" component={UserLayout}>
+    //       <Route path="/dashboard" component={Dashboard}>
+    //         <Route path="/hospital" component={HospitalHome} />
+    //       </Route>
 
-const accountTypeFilters = {
-  accountType: ["personal", "professional", "hospital"] as AccountType[]
-}
-const profileSetupFilters = {
-  accountType: ["personal", "professional"] as AccountType[]
-}
-render(() =>
-  <Router>
+    //       <Route path="/records" component={DoctorPage} />
+    //     </Route> */}
+    //     <Route path="/hospital" component={HospitalLandingPage} />
+    //   </Route>
+    // </Router>
+    <Router>{routes}</Router>
+  ),
 
-    <Route path="/signup">
-      <Route path="/" component={Setup} />
-      <Route path="/:accountType/start" matchFilters={accountTypeFilters} component={Start} />
-      <Route path="/:accountType/verify" matchFilters={accountTypeFilters} component={Verification} />
-      <Route path="/:accountType/profile" matchFilters={profileSetupFilters} component={Profile} />
-      <Route path="/hospital/needs" component={Hospital} />
-    </Route>
-
-    <Route path=":accountType/dashboard" matchFilters={accountTypeFilters}>
-      <Route path="/" component={Home} />
-
-    </Route>
-
-    <Route path="/" component={App}>
-      {/** <Route path="/" component={Home} />
-      <Route path="/user" component={UserLayout}>
-        <Route path="/dashboard" component={Dashboard}>
-          <Route path="/hospital" component={HospitalHome} />
-        </Route>
-
-        <Route path="/records" component={DoctorPage} />
-      </Route> **/}
-    </Route>
-
-  </Router>,
-
-  root!)
+  root!
+);
---
+++ src/routes.tsx
@@ -0,0 +1,80 @@
+import path from "path";
+import AppLayout from "./layouts/AppLayout";
+import HospitalLandingPage from "./pages/HospitalLandingPage";
+import App from "./App";
+import { Setup } from "./pages/SignUp/Setup";
+import { Start } from "./pages/SignUp/Start";
+import { AccountType } from "./types";
+import Verification from "./pages/SignUp/Verification";
+import Profile from "./pages/SignUp/Profile";
+import Hospital from "./pages/SignUp/Hospital";
+import Home from "./pages/Home";
+
+// Custom types
+const accountTypeFilters = {
+  accountType: ["personal", "professional", "hospital"] as AccountType[],
+};
+const profileSetupFilters = {
+  accountType: ["personal", "professional"] as AccountType[],
+};
+
+/**
+ * Routes Configuration
+ *
+ */
+const routes = {
+  path: "",
+  children: [
+    {
+      path: "/",
+      component: AppLayout,
+      children: [
+        {
+          path: "/hospital",
+          component: HospitalLandingPage,
+        },
+        {
+          path: "/",
+          component: App,
+        },
+      ],
+    },
+    {
+      // Authentication Routes
+      path: "/signup",
+      children: [
+        {
+          path: "/",
+          component: Setup,
+        },
+        {
+          path: "/:accountType/start",
+          matchFilters: accountTypeFilters,
+          component: Start,
+        },
+        {
+          path: "/:accountType/verify",
+          matchFilters: accountTypeFilters,
+          component: Verification,
+        },
+        {
+          path: "/:accountType/profile",
+          matchFilters: profileSetupFilters,
+          component: Profile,
+        },
+        {
+          path: "/hospital/needs",
+          component: Hospital,
+        },
+      ],
+    },
+    {
+      // Filtered Routes
+      path: ":accountType/dashboard",
+      matchFilters: accountTypeFilters,
+      component: Home,
+    },
+  ],
+};
+
+export default routes;
